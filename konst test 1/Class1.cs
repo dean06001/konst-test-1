47d47a7ab13948954f3c66a7e249c2c48411490b
dean06001 <dean06001@utb.vaxjo.se>
2024-04-03 10:42:53 +0200

    Add project files.

---
+++ konst test 1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konst_test_1
+{
+    internal class Bil1
+    {
+        public string märke;
+        public int årsmodell;
+        public string färg;
+        public string kört;
+        public int sitplattser;
+
+        public Bil1(string Märke, int år, string color, string kör, int plattser)
+        {
+            märke = Märke;
+            årsmodell = år;
+            färg = color;
+            kört = kör;
+            sitplattser = plattser;
+        }
+    }
+}
---
+++ konst test 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using konst_test_1;
+using System;
+namespace konst_test
+{
+    class Program
+    {
+        static void Main()
+        {
+            Bil1 bil = new Bil1("volvo", 2013, "svart", "500mil", 4);
+
+            Console.WriteLine(bil.märke);
+            Console.WriteLine(bil.årsmodell);
+            Console.WriteLine(bil.färg);
+            Console.WriteLine(bil.kört);
+            Console.WriteLine(bil.sitplattser);
+
+            Console.ReadLine();
+        }
+    }
+}
